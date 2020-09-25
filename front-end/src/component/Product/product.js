c6f4a3b2658013c26e3bbfb893631a15324dd260
Summerdise <350464517@qq.com>
2020-09-25 17:46:25 +0800

    [zmc] feat: add butoon on click function'

--- front-end/src/component/Product/product.js
+++ front-end/src/component/Product/product.js
@@ -4,12 +4,11 @@ class Product extends Component {
     render() {
         return (
             <Fragment>
-
                 <div>
                     <img src={this.props.url} alt="product_image" />
                     <p>{this.props.name}</p>
                     <p>单价：{this.props.price}元/{this.props.unit}</p>
-                    <button className="add_button"></button>
+                    <button className="add_button" onClick={this.props.handleAddProductAmount}></button>
                 </div>
             </Fragment>
         )
--- front-end/src/component/Shop/shop.js
+++ front-end/src/component/Shop/shop.js
@@ -1,53 +1,79 @@
-import React, { Fragment } from 'react';
+import React, { Fragment,Component } from 'react';
 import Product from '../Product/product';
 
-const Shop = (props) => {
-    const state = {
+class Shop extends Component {
+    state = {
         description: [
             {
                 id: 1,
                 name: '可乐1',
                 price: 15,
                 unit:"瓶",
-                url:   `https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2370172978,1480734805&fm=26&gp=0.jpg`
+                url:   `https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2370172978,1480734805&fm=26&gp=0.jpg`,
+                amount: 0
             },
             {
                 id: 2,
                 name: '可乐2',
                 price: 13,
                 unit:"罐",
-                url:   `https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2370172978,1480734805&fm=26&gp=0.jpg`
+                url:   `https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2370172978,1480734805&fm=26&gp=0.jpg`,
+                amount: 0
             },
             {
                 id: 3,
                 name: '可乐3',
                 price: 1,
                 unit:"瓶",
-                url:   `https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2370172978,1480734805&fm=26&gp=0.jpg`
+                url:   `https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2370172978,1480734805&fm=26&gp=0.jpg`,
+                amount: 0
             },
             {
                 id: 4,
                 name: '可乐4',
                 price: 10,
                 unit:"桶",
-                url:   `https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2370172978,1480734805&fm=26&gp=0.jpg`
+                url:   `https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2370172978,1480734805&fm=26&gp=0.jpg`,
+                amount: 0
             },
         ],
     };
-    return (
-        <Fragment>
-            <div>
-                {state.description.map((description) => (
-                    <Product
-                        name={description.name}
-                        price={description.price}
-                        unit={description.unit}
-                        url = {description.url}
-                    />
-                ))}
-            </div>
-        </Fragment>
-    )
+
+    handleAddProductAmount = (id) =>{
+        const description = this.state.description.map((des)=>{
+            if(des.id===id){
+                return {
+                    ...des,
+                    amount: Number(des.amount)+1
+                }
+            }else{
+                return {...des};
+            }
+        });
+
+        this.setState({
+            description,
+        })
+    };
+
+    render(){
+        return (
+            <Fragment>
+                <div>
+                    {this.state.description.map((description) => (
+                        <Product
+                            name={description.name}
+                            price={description.price}
+                            unit={description.unit}
+                            url = {description.url}
+                            amount = {description.amount}
+                            handleAddProductAmount={()=>this.handleAddProductAmount(description.id)}
+                        />
+                    ))}
+                </div>
+            </Fragment>
+        )
+    }
 }
 
 export default Shop;
